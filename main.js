975dfb68c2a16493c6702cdfd8a5509d14b76387
Waeyword <walldoyeamafaye@gmail.com>
2022-06-23 21:57:03 +0100

    first js commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let myName = "Wallace"
+let myHeight = 1.85
+let myCountry = "Nigeria"
+
+alert("My Name is Wallace, I am 6 ft 1 inch tall and I am from Nigeria")
+
+console.log("My name is", myName)
+console.log("I am", myHeight, "meters tall")
+console.log("I am from", myCountry)
